6209f551092c81f6b2915c17675a7589310b01a2
andreBass1201 <andre300.av@gmail.com>
2023-03-14 13:01:41 -0500

    aggregar proyectos introductorios

---
+++ Casting/src/Casting.java
@@ -0,0 +1,17 @@
+public class Casting {
+    public static void main(String[] args) {
+
+        double mounthDogs =30.0 / 12.0;
+
+        System.out.println(mounthDogs);
+
+        //COMVERTIRLO A UN ENTERO
+        int estimation = (int) mounthDogs;
+        System.out.println(estimation);
+
+        int a = 30;
+        int b = 12;
+
+        System.out.println((double)a/b);
+    }
+}
